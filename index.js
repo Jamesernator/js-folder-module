19971eb3125010c557b6ed4c2f1e7e97a849fd97
Julien Deniau <julien.deniau@gmail.com>
2017-06-13 12:13:35 +0200

    add slug

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 #!/usr/bin/env node
-const process = require('process')
-const fs = require('fs')
-const path = require('path')
-const mkdirp = require('mkdirp')
+const process = require('process');
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const camelCase = require('camelcase');
 
-const SRC_DIR = process.argv[2]
+const SRC_DIR = process.argv[2];
 if (!SRC_DIR) {
-    throw new Error(`No source directory given`)
+  throw new Error(`No source directory given`);
 }
 const OUTFILE = process.argv[3]
-    ? process.argv[3]
-    // This removes any trailing / in the path name
-    : './' + path.format(path.parse(SRC_DIR)) + '.js'
+  ? process.argv[3]
+  : // This removes any trailing / in the path name
+    './' + path.format(path.parse(SRC_DIR)) + '.js';
 
-mkdirp.sync(path.dirname(OUTFILE, 0o755))
-fs.writeFileSync(OUTFILE, '')
+mkdirp.sync(path.dirname(OUTFILE, 0o755));
+fs.writeFileSync(OUTFILE, '');
 
 // and build the export statements
-const files = fs.readdirSync(SRC_DIR)
+const files = fs.readdirSync(SRC_DIR);
 for (const file of files) {
-    const p = path.parse(file)
-    // Get the path relative to the OUTFILE so that exports are correct
-    const srcDir = path.relative(path.dirname(OUTFILE), SRC_DIR)
-    const _import = `import { default as _${ p.name } } from "./${ path.posix.join(srcDir, file) }"\n`
-    const _export = `export { _${ p.name } as ${ p.name } }\n`
-    fs.appendFileSync(OUTFILE, _import)
-    fs.appendFileSync(OUTFILE, _export)
+  const p = path.parse(file);
+  // Get the path relative to the OUTFILE so that exports are correct
+  const srcDir = path.relative(path.dirname(OUTFILE), SRC_DIR);
+  const slug = camelCase(p.name);
+  const _import = `import { default as _${slug} } from "./${path.posix.join(
+    srcDir,
+    file
+  )}"\n`;
+  const _export = `export { _${slug} as ${slug} }\n`;
+  fs.appendFileSync(OUTFILE, _import);
+  fs.appendFileSync(OUTFILE, _export);
 }
